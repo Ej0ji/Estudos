2c207a35a457f45958a3b206739a96d9cd8b7235
Eric Joji <ericjoji303@gmail.com>
2026-04-12 22:18:54 -0300

    Clean binary_search exercise
    Ajustes finos + documentação e organização de código

--- learning-python-algos/binary_search.py
+++ learning-python-algos/binary_search.py
@@ -1,41 +1,46 @@
-import time
-
-
-    # Remova o '#' do time.sleep(), caso queira debuggar a busca binária
-    # time.sleep(5)
+# Biblioteca local com o dicionário de nomes genéricos
+from name_dict import *
 
 def buscaBinaria(iniLista, fimLista): # -> int
 
     '''
-    Algoritmo de busca binária possui complexidade de execução O(log2n)
+    Algoritmo de busca binária para dicionários que possuem chaves (como índices numéricos - int) e valores (como nomes - strings).
+
+    iniLista -> Parâmetro de entrada para gerar o ínicio da lista do intervalo
+
+    fimLista -> Parâmetro de entrada para gerar o fim da lista do intervalo
+
     '''
 
-    # Preenchimento da lista com valores de parâmetros da função
-    lista = []
+    # Preenchimento da lista com o intervalo gerado a partir dos parâmetros de entrada da função
+    intervaloDeIndices = []
 
     for itensLista in range(iniLista, (fimLista + 1)):
-        lista.append(itensLista)
+        intervaloDeIndices.append(itensLista)
 
     # Declaração de variáveis da função
-    indicePrincipal = 0
-    indiceMax = max(lista)
-    indiceMin = min(lista)
+    indicePrincipal = 0 
+    indiceMax = max(intervaloDeIndices)
+    indiceMin = min(intervaloDeIndices)
     sinal = 0
-    contadorDeLoops = 0
+    contadorDeLoops = 0 
 
-    # Iteração da busca binária - Terminará quando o valor a ser buscado dentro da lista for encontrado, caso ocorra uma exceção será retornado um erro descritivo
+    # Iteração da busca binária - Terminará apenas quando o valor a ser buscado dentro da lista for encontrado, caso ocorra uma exceção será retornado um erro
     while True:
         try:
+            # Nome a ser buscado dentro do dicionário de nomes genéricos
+            nomeASerBuscado = input("Digite um nome: ")
+            # Índice do nome a ser buscado dentro do dicionário (caso o nome inserido não exista dentro do dicionário, a variável receberá o valor 'None')
+            indiceASerBuscado = next((chave for chave, valor in dicionarioGenerico.items() if valor == nomeASerBuscado), None)
 
-             # Variável de valor desejado para busca dentro do intervalo definido dentro da lista
-            valorASerBuscado = int(input(f"Digite um valor inteiro de {indiceMin} a {indiceMax} para localizar o elemento na lista: "))
-
-            if valorASerBuscado not in lista:
+            # Se o índice do nome a ser buscado não existir, não terá como encontrá-lo dentro do dicionário. Para isso uma exception é criada
+            if indiceASerBuscado == None:
                 raise Exception("O valor a ser buscado não está presente no intervalo!")
 
-            while indicePrincipal != valorASerBuscado:
+            # Enquanto o indice de busca ('indicePrincipal') não for o mesmo que o indice a ser buscado ('indiceASerBuscado'), o loop para encontrar o indice específico ocorrerá
+            while indicePrincipal != indiceASerBuscado:
 
-                contadorDeLoops += 1
+                contadorDeLoops += 1 # Será usado para verificar, ao final, a quantidade loops que foram necessários para encontrar o índice do nome
 
                 # Materialização da lista temporária que varia conforme o afunilamento do intervalo
                 listaTmp = []
@@ -43,7 +48,7 @@ def buscaBinaria(iniLista, fimLista): # -> int
                 for itensListaTmp in range(indiceMin, (indiceMax + 1)):
                     listaTmp.append(itensListaTmp)
             
-                # Bloco de modificação do indice principal conforme 
+                # Bloco de alteração do indice de busca ('indicePrincipal') conforme afunilamento dos intervalos da lista temporária ('listaTmp')
                 if indiceMin != 1 and sinal == 0:
                     indicePrincipal = indicePrincipal + int(((max(listaTmp) + 1) - min(listaTmp)) / 2)
                 elif indiceMin != 1 and sinal == 1:
@@ -51,33 +56,36 @@ def buscaBinaria(iniLista, fimLista): # -> int
                 else:
                     indicePrincipal = int(max(listaTmp) / 2)
 
-                if indicePrincipal > valorASerBuscado:
+                # Bloco de verificação de proximidade do indice de busca ('indicePrincipal') com o indice a ser buscado ('indiceASerBuscado')
+                if indicePrincipal > indiceASerBuscado:
 
                     indiceMax = indicePrincipal
                     sinal = 1
                     continue
-                
-                if indicePrincipal < valorASerBuscado:
+
+                if indicePrincipal < indiceASerBuscado:
 
                     indiceMin = indicePrincipal
                     sinal = 0
                     continue
-
-            meuDicionario = {
+            
+            # Dicionario com o indice correspondente ao nome buscado no dicionário de nomes genéricos ('dicionarioGenerico') e quantidade de loops ocorridos para encontrar o nome
+            dadosIndice = {
                     "indice" : indicePrincipal,
                     "loops" : contadorDeLoops
             }
 
-            return meuDicionario
+            return dadosIndice
             
         except Exception as e:
             print(f"Ocorreu um erro: {e}")
         
+# Teste unitário da entrada, chamada e saída da função de busca binária
+if __name__ == "__main__":
 
-# Aprecie o resultado com moderação...
-inicioIntervalo = int(input('Escolha um número para ser o ínicio do intervalo: '))
-fimIntervalo = int(input('Escolha um número para ser o fim do intervalo: '))
-print(buscaBinaria(inicioIntervalo, fimIntervalo))
+    inicioIntervalo = min(dicionarioGenerico)
+    fimIntervalo = max(dicionarioGenerico)
+    print(buscaBinaria(inicioIntervalo, fimIntervalo))
 
 
 
